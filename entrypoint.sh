14548fd6d20dc0d301327339357f32451bb5e582
kukam <dusan.grmela@gmail.com>
2018-02-22 10:53:06 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
-PWEHOST=`/sbin/ip route|awk '/default/ { print $3 }'`
+#PWEHOST=`/sbin/ip route|awk '/default/ { print $3 }'`
 PWEROOT=${PWE-CONF-pwe-home:-"/PWE/examples/static_web"}
 
 sed -i "s@/PWEROOT@$PWEROOT@g" /etc/nginx/conf.d/default.conf
-sed -i "s@PWEHOST@$PWEHOST@g" /etc/nginx/conf.d/default.conf
+#sed -i "s@PWEHOST@$PWEHOST@g" /etc/nginx/conf.d/default.conf
 
-nginx -g "daemon off; error_log stderr info;"
+exec nginx -g "daemon off; error_log stderr info;"
